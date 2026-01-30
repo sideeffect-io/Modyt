e5995543127bad95f896541f74b5ea44c74b2e96
Thibault Wittemberg <thibault.wittemberg@gmail.com>
2026-01-30 17:53:24 +0100

    Initial Commit

---
+++ MoDyt/MoDytApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoDytApp.swift
+//  MoDyt
+//
+//  Created by Thibault Wittemberg on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoDytApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoDytTests/MoDytTests.swift
@@ -0,0 +1,16 @@
+//
+//  MoDytTests.swift
+//  MoDytTests
+//
+//  Created by Thibault Wittemberg on 30/01/2026.
+//
+
+import Testing
+
+struct MoDytTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
